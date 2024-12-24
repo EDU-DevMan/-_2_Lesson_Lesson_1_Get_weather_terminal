efc50bbce4ec58e2b3b72be46b371538c106a1e8
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2024-12-24 18:03:36 +0300

    I replaced sity on city

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ CITIES = ["Лондон", "Шереметьево", "Череповец"]
 def main():
     payload = {"nMTq": "", 'lang': 'ru'}
 
-    for sity in CITIES:
+    for city in CITIES:
         url_template = 'https://wttr.in/{}'
-        url = url_template.format(sity)
+        url = url_template.format(city)
         response = requests.get(url, params=payload)
         print(response.text)
 
