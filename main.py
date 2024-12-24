9a3fa03060d542e87a5117ba011c691d02526905
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2024-12-24 10:57:48 +0300

    I have corrected the name of the variables

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 
 
-OFFICES = ["Лондон", "Шереметьево", "Череповец"]
+CITIES = ["Лондон", "Шереметьево", "Череповец"]
 
 
 def main():
-    for office in OFFICES:
+    for sity in CITIES:
         url_template = 'https://wttr.in/{}?nMTq&lang=ru'
-        url = url_template.format(office)
+        url = url_template.format(sity)
         response = requests.get(url)
         print(response.text)
 
