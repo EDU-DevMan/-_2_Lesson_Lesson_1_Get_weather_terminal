0a5f4b94b9cc1b84b747df3211e1b9120400c00b
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2024-12-27 16:00:42 +0300

    I added try-except and logging.info

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 
 CITIES = ["Лондон", "Шереметьево", "Череповец"]
@@ -7,11 +8,34 @@ CITIES = ["Лондон", "Шереметьево", "Череповец"]
 def main():
     payload = {"nMTq": "", 'lang': 'ru'}
 
-    for city in CITIES:
-        url_template = 'https://wttr.in/{}'
-        url = url_template.format(city)
-        response = requests.get(url, params=payload)
-        print(response.text)
+    try:
+        for city in CITIES:
+            url_template = 'https://wttr.in/{}'
+            url = url_template.format(city)
+            response = requests.get(url, params=payload)
+            response.raise_for_status()
+            print(response.text)
+
+    except requests.exceptions.HTTPError:
+        logging.basicConfig(format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
+        logging.info("Not Found")
+    except requests.exceptions.ReadTimeout:
+        logging.basicConfig(format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
+        logging.info("Request Timeout")
+    except requests.exceptions.ConnectionError:
+        logging.basicConfig(format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
+        logging.info("Connection error")
+    except requests.exceptions.RequestException:
+        logging.basicConfig(format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
+        logging.info("Exception request")
 
 
 if __name__ == '__main__':
