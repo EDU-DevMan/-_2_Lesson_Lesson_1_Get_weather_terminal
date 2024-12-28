2304c69d4b3ca251e3d053edf417c7d68bcaa555
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2024-12-28 11:56:22 +0300

    I have redone the check statuses

--- main.py
+++ main.py
@@ -13,19 +13,16 @@ def main():
             url_template = 'https://wttr.in/{}'
             url = url_template.format(city)
             response = requests.get(url, params=payload)
-            response.raise_for_status()
-            print(response.text)
+            if response.status_code == 200:
+                print(response.text)
+            else:
+                print("Not Found.")
 
-    except requests.exceptions.HTTPError:
-        logging.basicConfig(format='%(asctime)s - %(message)s',
-                            datefmt='%d-%b-%y %H:%M:%S',
-                            level=logging.INFO)
-        logging.info("Not Found")
     except requests.exceptions.ConnectionError:
         logging.basicConfig(format='%(asctime)s - %(message)s',
                             datefmt='%d-%b-%y %H:%M:%S',
                             level=logging.INFO)
-        logging.info("Connection error")
+        logging.info("Connection error.")
 
 
 if __name__ == '__main__':
