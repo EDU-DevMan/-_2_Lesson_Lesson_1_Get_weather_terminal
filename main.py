ba18920dd53e06b2eaa0bfa24b9a35e2fc28f581
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2024-12-28 12:19:09 +0300

    I added func request_weather

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ import logging
 CITIES = ["Лондон", "Шереметьево", "Череповец"]
 
 
-def main():
+def request_weather():
     payload = {"nMTq": "", 'lang': 'ru'}
 
-    try:
-        for city in CITIES:
-            url_template = 'https://wttr.in/{}'
-            url = url_template.format(city)
-            response = requests.get(url, params=payload)
-            if response.status_code == 200:
-                print(response.text)
-            else:
-                print("Not Found.")
+    for city in CITIES:
+        url_template = 'https://wttr.in/{}'
+        url = url_template.format(city)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
 
+
+def main():
+    try:
+        request_weather()
+    except requests.exceptions.HTTPError:
+        logging.basicConfig(format='%(asctime)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.INFO)
+        logging.info("Not Found")
     except requests.exceptions.ConnectionError:
         logging.basicConfig(format='%(asctime)s - %(message)s',
                             datefmt='%d-%b-%y %H:%M:%S',
